e22901ca64b3a87632e614e10fae82699cbee76f
prathamesh69420 <133740038+prathamesh69420@users.noreply.github.com>
2023-05-16 11:35:08 +0530

    added dynamic users to performance_teller.sh

--- performance_teller.sh
+++ performance_teller.sh
@@ -5,14 +5,11 @@ current_perf=$(cat /proc/acpi/call | tr '\0' '\n' | sed -e s/DBUS_SESSION_BUS_AD
 
 if [[ "$current_perf" == '0x0' ]]; then
 	echo "Intelligent Cooling"
-	sudo -u knight DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u knight)/bus notify-send "Intelligent Cooling" "Balance is essential in life"
 
 elif [[ "$current_perf" == '0x1' ]]; then
 	echo "Extreme Performance"
-	sudo -u knight DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u knight)/bus notify-send "Extreme Performance" "Gaming, huh?"
 
 elif [[ "$current_perf" == '0x2' ]]; then
 	echo "Battery Saving"
-	sudo -u knight DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u knight)/bus notify-send "Battery Saving" "Save Battery, Save Lives"
 
 fi
